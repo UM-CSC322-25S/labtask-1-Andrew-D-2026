2dfeabead62d43ccbc8b515b5e637e7eb85aba6d
Dietel <acd92956@miami.edu>
2025-02-01 18:27:57 -0500

    added 3 more files

---
+++ GetMyStuff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "User ID: $(id -u)"
+echo "Primary Group ID: $(id -g)"
+echo "Groups: $(groups)"
+echo "Absoulte Shell Path: $SHELL"
+echo "Absoulte Home Directory Path: $HOME"
+echo "PATH Environment Variable: $PATH"
+
